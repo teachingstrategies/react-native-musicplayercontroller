d45f995739a1287baf3e698e9bec1d3e4c1cff0e
Joaquin <joaqman@yahoo.com>
2021-01-22 14:26:54 -0800

    Add new control methods
    Add methods to skip, previous and to get song info.

--- ios/RNReactNativeMusicplayercontroller.m
+++ ios/RNReactNativeMusicplayercontroller.m
@@ -137,6 +137,52 @@ RCT_EXPORT_METHOD(playMusic: (RCTResponseSenderBlock)callback) {
     }
 }
 
+RCT_EXPORT_METHOD(skipSong) {
+     if (musicPlayer != nil) {
+         [musicPlayer skipToNextItem];
+     }
+ }
+
+ RCT_EXPORT_METHOD(skipToPreviousSong) {
+     if (musicPlayer != nil) {
+         [musicPlayer skipToPreviousItem];
+     }
+ }
+
+ RCT_EXPORT_METHOD(getArtist:(RCTPromiseResolveBlock)resolve
+                reject:(RCTPromiseRejectBlock)reject) {
+     if(musicPlayer != nil) {
+        MPMediaItem *currentItem = musicPlayer.nowPlayingItem;
+        NSString *artist = [currentItem valueForProperty:MPMediaItemPropertyArtist];
+        resolve(artist);
+     } else {
+        resolve(@"error");
+     }
+ }
+
+ RCT_EXPORT_METHOD(getSong:(RCTPromiseResolveBlock)resolve
+                reject:(RCTPromiseRejectBlock)reject) {
+     if(musicPlayer != nil) {
+        MPMediaItem *currentItem = musicPlayer.nowPlayingItem;
+        NSString *artist = [currentItem valueForProperty:MPMediaItemPropertyTitle];
+        resolve(artist);
+     } else {
+        resolve(@"error");
+     }
+ }
+
+ RCT_EXPORT_METHOD(getProgress:(RCTPromiseResolveBlock)resolve
+                reject:(RCTPromiseRejectBlock)reject) {
+     if(musicPlayer != nil) {
+        double currentDuration = [[[musicPlayer nowPlayingItem] valueForProperty:MPMediaItemPropertyPlaybackDuration]doubleValue];
+        double currentTime = (double) [musicPlayer currentPlaybackTime];
+        double progress = currentTime/currentDuration;
+        resolve([NSNumber numberWithDouble:progress]);
+     } else {
+        resolve(@"error");
+     }
+}
+
 RCT_EXPORT_METHOD(stopMusic: (RCTResponseSenderBlock)callback) {
     if (musicPlayer != nil) {
         callback(@[[NSNumber numberWithInteger:0], @[]]);
